bf2be130bfd1154252725b5e5ee65bc3a2261303
stepanjennaop <stepanjennaop@gmail.com>
2026-06-02 13:29:42 +0300

    commit

--- fenwicktree/fenwicktree.go
+++ fenwicktree/fenwicktree.go
@@ -1,14 +1,1 @@
 package fenwicktree
-
-type Monoid[T any] struct {
-	Identity T
-	Combine  func(a, b T) T
-	Inverse  func(a T) T
-}
-
-type FenwickTree[T any] struct {
-	tree   []T
-	data   []T
-	size   int
-	monoid Monoid[T]
-}
--- segmenttree/segmenttree.go
+++ segmenttree/segmenttree.go
@@ -1,1 +1,28 @@
 package segmenttree
+
+type SegmentTree[T any] struct {
+	n    int
+	tree []T
+	op   func(a, b T) T
+	id   T
+}
+
+func New[T any](n int, op func(a, b T) T, id T) *SegmentTree[T] {
+	return &SegmentTree[T]{
+		n:    n,
+		tree: make([]T, 4*n),
+		op:   op,
+		id:   id,
+	}
+}
+
+func (st *SegmentTree[T]) Build(arr []T) {
+	st.build(1, 0, st.n-1, arr)
+}
+
+func (st *SegmentTree[T]) build(v, tl, tr int, arr []T) {
+	if tl == tr {
+		st.tree[v] = arr[tl]
+		return
+	}
+}
